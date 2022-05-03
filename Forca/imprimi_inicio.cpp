ad3ba3906623cb43f5a1c747769bb5de0899dc4a
luiz <luiz49138@gmail.com>
2022-05-02 22:43:35 -0300

    compilação de arquivo objeto

---
+++ Forca/imprimi_inicio.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+
+void imprimi_inicio(){
+    std :: cout << "****************************" << std :: endl;
+    std :: cout << "* Bora adivinhar a FRUTA? **" << std :: endl;
+    std :: cout << "****************************" << std :: endl;
+    std :: cout << std :: endl;
+    
+} 
