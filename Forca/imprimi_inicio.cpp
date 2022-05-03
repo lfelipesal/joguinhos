673668030fc73c1c8adabb2f75678ddd3a9881fc
luiz <luiz49138@gmail.com>
2022-05-02 23:56:25 -0300

    Mais correção

--- Forca/imprimi_inicio.cpp
+++ Forca/imprimi_inicio.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-
-
 void imprimi_inicio(){
     std :: cout << "****************************" << std :: endl;
     std :: cout << "* Bora jogar forca ? *******" << std :: endl;
